3634dbb2467ed21812bf87445619fa9dfd584428
2ym <78376740+2ym@users.noreply.github.com>
2023-10-01 22:26:53 +0200

    Update lge.ps1

--- lge.ps1
+++ lge.ps1
@@ -3,7 +3,7 @@ Start-Transcript $ENV:TEMP\lge.log -Append
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "LGE muss als Administrator ausgeführt werden. LGE startet neu als Administrator..." -ForegroundColor "Cyan"
-    Start-Process -Verb runas -FilePath "C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://is.gd/HRUZnv | iex" -NoExit
+    Start-Process -Verb runas -FilePath "C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "-NoExit iwr -useb https://is.gd/HRUZnv | iex"
     break
 }
 
@@ -194,3 +194,4 @@ Get-AppXProvisionedPackage -Online | Where DisplayName -like "Microsoft.ZuneVide
 # Uninstall Windows Media Player
 Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue | Out-Null
 
+Read-Host -Prompt "Press Enter to exit"
