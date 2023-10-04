59c022b5545893ee70db2c1f198c1004b5ea80e9
2ym <78376740+2ym@users.noreply.github.com>
2023-10-04 18:34:02 +0200

    Update main0.ps1

--- main0.ps1
+++ main0.ps1
@@ -1,6 +1,10 @@
 $winbuild_s = 22621
 $winbuild_i = [System.Environment]::OSVersion.Version.Build
 $username_i = [System.Environment]::UserName
+$organization = "GCE Bayreuth"
+$username_1 = "Admin"
+$username_2 = "Lehrer"
+$username_3 = "Schueler"
 
 if ( $winbuild_i -eq $winbuild_s ) {
     Write-Host "Windows Build $winbuild_s detected." -ForegroundColor "Cyan"
@@ -9,12 +13,15 @@ if ( $winbuild_i -eq $winbuild_s ) {
     break
 }
 
-if ( $username_i -eq "Admin" ) {
-    Write-Host "Admin Account detected." -ForegroundColor "Cyan"
-    Start-Process -Verb runas -FilePath "C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/admin.ps1 | iex"
-} elseif ( $username_i -eq "Lehrer" ) {
-    Write-Host "Lehrer Account detected." -ForegroundColor "Cyan"
-    Start-Process -Verb runas -FilePath "C:\Users\Lehrer\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/lehrer.ps1 | iex"
+if ( $username_i -eq $username_1 ) {
+    Write-Host "$username_1 Account detected." -ForegroundColor "Cyan"
+    Start-Process -Verb runas -FilePath "C:\Users\$username_1\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/admin.ps1 | iex"
+} elseif ( $username_i -eq $username_2 ) {
+    Write-Host "$username_2 Account detected." -ForegroundColor "Cyan"
+    Start-Process -Verb runas -FilePath "C:\Users\$username_2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/lehrer.ps1 | iex"
+} elseif ( $username_i -eq $username_3 ) {
+    Write-Host "$username_3 Account detected." -ForegroundColor "Cyan"
+    Start-Process -Verb runas -FilePath "C:\Users\$username_3\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/lehrer.ps1 | iex"
 } else {
     Write-Host "No valid Account Name detected." -ForegroundColor "Red"
     break
