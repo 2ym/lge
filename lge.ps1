a4efe6b699fe17c9f63c4eabf0fbe963b4a1d3fd
2ym <78376740+2ym@users.noreply.github.com>
2023-10-07 13:13:18 +0200

    Update lge.ps1

--- lge.ps1
+++ lge.ps1
@@ -30,13 +30,13 @@ Write-Host "###########################" -ForegroundColor "Blue"
 Write-Host ""
 
 if ( $user_i -eq $user_1 ) {
-    Write-Host "$user_1 Account detected." -ForegroundColor "Green"
+    Write-Host "$user_1 Account detected." -ForegroundColor "Cyan"
     # Start-Process -Verb runas -FilePath "C:\Users\$user_1\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/admin.ps1 | iex"
 } elseif ( $user_i -eq $user_2 ) {
-    Write-Host "$user_2 Account detected." -ForegroundColor "Green"
+    Write-Host "$user_2 Account detected." -ForegroundColor "Cyan"
     # Start-Process -Verb runas -FilePath "C:\Users\$user_2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/lehrer.ps1 | iex"
 } elseif ( $user_i -eq $user_3 ) {
-    Write-Host "$user_3 Account detected." -ForegroundColor "Green"
+    Write-Host "$user_3 Account detected." -ForegroundColor "Cyan"
     # Start-Process -Verb runas -FilePath "C:\Users\$user_3\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" -ArgumentList "iwr -useb https://raw.githubusercontent.com/2ym/lge/main/lehrer.ps1 | iex"
 } else {
     Write-Host "No valid Account Name detected." -ForegroundColor "Red"
